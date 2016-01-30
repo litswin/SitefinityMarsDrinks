1b7d240a7280da76266794373506593c7411cdab
litswin <litswin@gmail.com>
2016-01-30 18:46:25 +0300

    real initial commit part 1

---
+++ Global.asax.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Telerik.Microsoft.Practices.Unity;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Personalization;
+using Telerik.Sitefinity.Personalization.Impl;
+
+namespace SitefinityWebApp
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Initialized += Bootstrapper_Initialized;
+        }
+
+        private void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
+        {
+            if (e.CommandName == "Bootstrapped")
+            {
+                ObjectFactory.Container.RegisterType(
+                    typeof(ICriterionEvaluator),
+                    typeof(CustomEvaluator),
+                    PersonalizationConstants.CriteriaName.SearchKeywords,
+                    new ContainerControlledLifetimeManager(),
+                    new InjectionConstructor());
+            }
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
